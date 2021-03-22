e63c5c8133f07187df04d72b5f36e832e78ee343
Johnny Saraiva <johnny.saraiva.dev@gmail.com>
2021-03-22 07:41:14 -0300

    feat: Update Input file to use typescript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,19 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
-
 import { useField } from '@unform/core';
-
+import {
+  useCallback, useEffect,
+  useRef,
+  useState
+} from 'react';
+import { IconBaseProps } from 'react-icons/lib';
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>
+}
+
+function Input({ name, icon: Icon, ...rest }: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
